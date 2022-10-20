3b28e378d94e2237824f03d3e6aca00b7a494562
Polushin Dima <dima2004polushin2004@gmail.com>
2022-10-20 14:18:40 +0600

    Общий алгоритм лотереи

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Lottery from "./Circle/circle";
 
 function App() {
+  let [num, setNum] = useState<number[]> ([]);
+
+  const random = (min:number, max:number) => {
+    return Math.floor(min + Math.random() * (max + 1 - min));
+  }
+
+  const getNumberLottery = () => {
+    setNum([
+      num[0] = random(5, 36),
+      num[1] = random(5, 36),
+      num[2] = random(5, 36),
+      num[3] = random(5, 36),
+      num[4] = random(5, 36),
+    ].sort((a, b) => a - b));
+  };
+
+  const numberLottery = num.filter((x, i) => num.indexOf(x) === i);
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getNumberLottery}>Крутануть</button>
+      <div className='lottery'>
+        <Lottery number={numberLottery[0]}/>
+        <Lottery number={numberLottery[1]}/>
+        <Lottery number={numberLottery[2]}/>
+        <Lottery number={numberLottery[3]}/>
+        <Lottery number={numberLottery[4]}/>
+      </div>
     </div>
   );
 }
